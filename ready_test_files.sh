b6dcf78be1731b425287f3607c24ec7c90d5b30c
coyuki <fu08yu20ko07@icloud.com>
2025-02-15 01:38:52 +0900

    *not working*

---
+++ ready_test_files.sh
@@ -0,0 +1,3 @@
+rm -r ./assets_for_test/assets && echo "removed old assets"
+mkdir ./assets_for_test/assets
+cp ./assets_for_test/source/* ./assets_for_test/assets/ && echo "created new assets"
---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+use std::{
+    fmt::Display,
+    fs::{self, ReadDir},
+    io,
+    path::{Path, PathBuf},
+};
+// use thiserror::Error;
+
+#[derive(Debug)]
+pub enum T2LError {
+    IoError(io::Error),
+    SomeError,
+}
+impl Display for T2LError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            T2LError::IoError(e) => write!(f, "{}", e),
+            T2LError::SomeError => write!(f, "someerror"),
+        }
+    }
+}
+
+struct PathSet {
+    old: PathBuf,
+    new: PathBuf,
+}
+
+pub struct PathSets {
+    list: Vec<PathSet>,
+}
+impl PathSets {
+    pub fn new<P: AsRef<Path>>(dir: P) -> Result<Self, T2LError> {
+        let mut list = Self { list: Vec::new() };
+        let file_list = get_file_list(dir)?;
+
+        for f in file_list {
+            let entry_path = f.map_err(T2LError::IoError)?.path();
+            let ext = match entry_path.extension() {
+                Some(e) => match e.to_str() {
+                    Some(ee) => ee,
+                    None => continue,
+                },
+                None => continue,
+            };
+            match ext {
+                "wav" => {
+                    let text_path = entry_path.parent().unwrap().join("text.txt"); // TODO: unwrap
+                    let text_content = fs::read_to_string(&text_path).map_err(T2LError::IoError)?;
+                    let audio_path = entry_path.join(format!("{}.mp3", &text_content[0..=20]));
+                    list.list.push(PathSet {
+                        old: entry_path,
+                        new: audio_path,
+                    });
+                } // make tool of getting extension someday.
+                _ => continue,
+            }
+        }
+        Ok(list)
+    }
+
+    pub fn get_check_list(&self) -> Vec<(String, String)> {
+        let mut tmp_list = Vec::<(String, String)>::new();
+        for set in &self.list {
+            tmp_list.push((
+                set.old.to_string_lossy().to_string(),
+                set.new.to_string_lossy().to_string(),
+            ));
+        }
+        tmp_list
+    }
+
+    /// Rename all paths in the list.
+    pub fn rename(&self) -> Result<(), T2LError> {
+        for set in &self.list {
+            fs::rename(&set.old, &set.new).map_err(T2LError::IoError)?;
+        }
+        Ok(())
+    }
+}
+
+// fn rename_audio(&mut )
+
+fn get_file_list<P: AsRef<Path>>(dir: P) -> Result<ReadDir, T2LError> {
+    Ok(fs::read_dir(dir).map_err(T2LError::IoError)?)
+}
+
+#[test]
+fn test_get_file_list() {
+    eprintln!("{:?}", get_file_list("assets_for_test/assets/"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::process;
+
+    #[test]
+    fn global_test() {
+        assert!(process::Command::new("sh")
+            .arg("ready_test_files.sh")
+            .status()
+            .unwrap()
+            .success());
+        let a = match PathSets::new("assets_for_test/assets/") {
+            Ok(a) => a,
+            Err(e) => {
+                eprintln!("{}", e);
+                panic!("Failed to create PathSets: {}", e)
+            }
+        };
+        let b = a.get_check_list();
+        for i in b {
+            println!("* {:width$} ---> {}", i.0, i.1, width = 20);
+        }
+        assert!(a.rename().is_ok());
+    }
+}
