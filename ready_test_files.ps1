da227f6edad94a4336bcd7fb21698dd369375e86
coyuki <fu08yu20ko07@icloud.com>
2025-02-24 21:27:58 +0900

    add get_file_list() and tests

---
+++ ready_test_files.ps1
@@ -0,0 +1,10 @@
+# this script has not yet been verified to Normal operation.
+Write-Output "this script has not yet been verified to Normal operation."
+
+Remove-Item -Path ".\assets_for_test\assets" -Recurse -Force
+Write-Output "removed old assets"
+
+New-Item -Path ".\assets_for_test\assets" -ItemType Directory
+
+Copy-Item -Path ".\assets_for_test\source\*" -Destination ".\assets_for_test\assets\" -Recurse
+Write-Output "created new assets"
--- src/lib.rs
+++ src/lib.rs
@@ -1,114 +1,89 @@
 use std::{
-    fmt,
-    fs::{self, ReadDir},
+    fs::{self, DirEntry},
     io,
-    path::{Path, PathBuf},
+    path::Path,
 };
 
 #[derive(Debug)]
-pub enum T2LError {
+pub enum Error {
     IoError(io::Error),
 }
-impl fmt::Display for T2LError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            T2LError::IoError(e) => write!(f, "{}", e),
-        }
+
+pub struct A<P: AsRef<Path>> {
+    audio: P,
+    line: P,
+}
+
+impl<P: AsRef<Path>> A<P> {
+    pub fn new(dir: P) -> Self {
+        todo!()
     }
 }
 
-struct PathSet {
-    old: PathBuf,
-    new: PathBuf,
-}
-
-pub struct PathSets {
-    list: Vec<PathSet>,
-}
-impl PathSets {
-    pub fn new<P: AsRef<Path>>(dir: P) -> Result<Self, T2LError> {
-        let mut list = Self { list: Vec::new() };
-        let file_list = get_file_list(dir)?;
-
-        for f in file_list {
-            let entry_path = f.map_err(T2LError::IoError)?.path();
-            let ext = match entry_path.extension() {
-                Some(e) => match e.to_str() {
-                    Some(ee) => ee,
-                    None => continue,
-                },
-                None => continue,
-            };
-            match ext {
-                "wav" => {
-                    let text_path = entry_path.parent().unwrap().join("text.txt"); // TODO: unwrap
-                    let text_content = fs::read_to_string(&text_path).map_err(T2LError::IoError)?;
-                    let audio_path = entry_path.join(format!("{}.mp3", &text_content[0..=20]));
-                    list.list.push(PathSet {
-                        old: entry_path,
-                        new: audio_path,
-                    });
-                }
-                _ => continue,
-            }
-        }
-        Ok(list)
-    }
-
-    pub fn get_check_list(&self) -> Vec<(String, String)> {
-        let mut tmp_list = Vec::<(String, String)>::new();
-        for set in &self.list {
-            tmp_list.push((
-                set.old.to_string_lossy().to_string(),
-                set.new.to_string_lossy().to_string(),
-            ));
-        }
-        tmp_list
-    }
-
-    /// Rename all paths in the list.
-    pub fn rename(&self) -> Result<(), T2LError> {
-        for set in &self.list {
-            fs::rename(&set.old, &set.new).map_err(T2LError::IoError)?;
-        }
-        Ok(())
-    }
-}
-
-// fn rename_audio(&mut )
-
-fn get_file_list<P: AsRef<Path>>(dir: P) -> Result<ReadDir, T2LError> {
-    Ok(fs::read_dir(dir).map_err(T2LError::IoError)?)
-}
-
-#[test]
-fn test_get_file_list() {
-    eprintln!("{:?}", get_file_list("assets_for_test/assets/"))
+fn get_file_list<P: AsRef<Path>>(dir: P) -> Result<Vec<DirEntry>, Error> {
+    let filtered_list: Vec<_> = fs::read_dir(dir)
+        .map_err(Error::IoError)?
+        .filter_map(|e| {
+            e.ok().filter(|ee| {
+                ee.path()
+                    .extension()
+                    .is_some_and(|n| n.to_str().unwrap() == "wav" || n.to_str().unwrap() == "txt")
+            })
+        })
+        .collect();
+    Ok(filtered_list)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::process;
+    use std::{env, process};
+
+    fn ready() -> bool {
+        let p = env::current_dir().unwrap().to_path_buf();
+        let windows_path = p.join("ready_test_files.ps1");
+        let unix_path = p.join("ready_test_files.sh");
+        if cfg!(target_os = "windows") {
+            if process::Command::new("powershell")
+                .args(["-NoExit", "-File", windows_path.to_str().unwrap()])
+                .status()
+                .unwrap()
+                .success()
+            {
+                println!("Ready!(windows)");
+                true
+            } else {
+                println!("Not ready!(windows)");
+                false
+            }
+        } else {
+            if process::Command::new("sh")
+                .args([unix_path.to_str().unwrap()])
+                .status()
+                .unwrap()
+                .success()
+            {
+                println!("Ready!(unix)");
+                true
+            } else {
+                println!("Not ready!(unix)");
+                false
+            }
+        }
+    }
 
     #[test]
-    fn global_test() {
-        assert!(process::Command::new("sh")
-            .arg("ready_test_files.sh")
-            .status()
+    fn a() {
+        ready();
+        println!(
+            "{:?}",
+            get_file_list(
+                env::current_dir()
+                    .unwrap()
+                    .join("assets_for_test")
+                    .join("assets")
+            )
             .unwrap()
-            .success());
-        let a = match PathSets::new("assets_for_test/assets/") {
-            Ok(a) => a,
-            Err(e) => {
-                eprintln!("{}", e);
-                panic!("Failed to create PathSets: {}", e)
-            }
-        };
-        let b = a.get_check_list();
-        for i in b {
-            println!("* {:width$} ---> {}", i.0, i.1, width = 20);
-        }
-        assert!(a.rename().is_ok());
+        );
     }
 }
